f869438d99a36b0cd4082888c503841901be037c
Allan Chaves <allancac@gmail.com>
2022-01-04 13:40:34 -0200

    Desaclop. dos layers presentation e domain da rota

--- routes/api/boleto.js
+++ routes/api/boleto.js
@@ -1,24 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { BoletoRouter } = require('./presentation/BoletoRouter')
 
 // @route   GET /boleto/:barCode
 // @desc    Rota para validar o código de barra de boletos de títulos bancários e pagamentos de concessionárias
 // @access  Public
-router.get('/:barCode', async (req, res) => {
-  //  Obter o código de brras do parâmetro fornecido na URI do endpoint /boleto
-  const barCode = req.params.barCode
-  //  Validar Código de Barras
-
-  try {
-    res.status(200).json({
-      barCode,
-      amount: '20.00',
-      expirationDate: '2018-07-16'
-    })
-  } catch (erro) {
-    console.log(erro.message)
-    res.status(500).send({ erros: [{ msg: 'Erro no servidor.' }] })
-  }
-})
+router.get('/:barCode', new BoletoRouter().route)
 
 module.exports = router
