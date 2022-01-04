6ed45a9a3a66b880703cfb88fe6b0a71a0c2667a
Allan Chaves <allancac@gmail.com>
2022-01-04 13:37:09 -0200

    Tratamento de chamadas inválidas na API

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const boleto = require('./routes/api/boleto')
 // Disponibilização das rotas pelo servidor
 app.use('/boleto', boleto)
 
+// Tratamento para requisições inválidas
+app.use((req, res, next) => {
+  // const erro = new Error('Rota não encontrada ou inexistente')
+  res.status(404).json({ erro: 'Rota não encontrada ou inexistente' })
+  next()
+})
+
 // Server Listening Service
 const port = process.env.PORT || 8080
 app.listen(port, () => {
