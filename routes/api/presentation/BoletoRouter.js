2f2e1c3baad7ff42e2f78dfab1f6b865953e3842
Allan Chaves <allancac@gmail.com>
2022-01-05 09:13:43 -0200

    limpeza de código

--- routes/api/presentation/BoletoRouter.js
+++ routes/api/presentation/BoletoRouter.js
@@ -10,14 +10,11 @@ class BoletoRouter {
       // Tratamento da resposta HTTP
       console.log(resultado[0].isValid)
       if (resultado[0].isValid === true) {
-        console.log('Retorno de quando o status é 200. Layer Apresentação')
         res.status(200).json(resultado[1])
       } else {
-        console.log('Retorno de quando o status é 400. Layer Apresentação')
         res.status(400).json(resultado[2])
       }
     } catch (erro) {
-      console.log('retorno do catch do BoletoRouter. Layer Apresentação')
       res.status(400).send(erro.message)
     }
   }
